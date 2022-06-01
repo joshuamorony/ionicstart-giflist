285853ee392d72dc1ec67bb13a9dbca737e95312
Joshua Morony <joshua.morony@gmail.com>
2022-06-01 20:27:31 +0930

    added load method that supplies dummy data

--- src/app/shared/data-access/reddit.service.spec.ts
+++ src/app/shared/data-access/reddit.service.spec.ts
@@ -1,14 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { subscribeSpyTo } from '@hirez_io/observer-spy';
+import { SubscriberSpy, subscribeSpyTo } from '@hirez_io/observer-spy';
+import { Gif } from '../interfaces/gif';
 
 import { RedditService } from './reddit.service';
 
 describe('RedditService', () => {
   let service: RedditService;
+  let getGifsSpy: SubscriberSpy<Gif[]>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(RedditService);
+    getGifsSpy = subscribeSpyTo(service.getGifs());
   });
 
   it('should be created', () => {
@@ -17,8 +20,14 @@ describe('RedditService', () => {
 
   describe('getGifs()', () => {
     it('should return a stream of an empty array initially', () => {
-      const observerSpy = subscribeSpyTo(service.getGifs());
-      expect(observerSpy.getLastValue()).toEqual([]);
+      expect(getGifsSpy.getLastValue()).toEqual([]);
+    });
+  });
+
+  describe('loadGifs()', () => {
+    it('should cause new data to emit on getGifs() stream', () => {
+      service.loadGifs();
+      expect(getGifsSpy.getLastValue()?.length).toBeGreaterThan(0);
     });
   });
 });
--- src/app/shared/data-access/reddit.service.ts
+++ src/app/shared/data-access/reddit.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { Gif } from '../interfaces/gif';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RedditService {
+  private gifs$ = new BehaviorSubject<Gif[]>([]);
+
   constructor() {}
 
   getGifs() {
-    return of([]);
+    return this.gifs$.asObservable();
+  }
+
+  loadGifs() {
+    const dummyData = [
+      { title: 'hello', url: 'https://google.com', src: '' },
+      { title: 'there', url: 'https://google.com', src: '' },
+    ];
+
+    this.gifs$.next(dummyData);
   }
 }
