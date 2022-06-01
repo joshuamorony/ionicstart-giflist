0e4730d1eddafe98564b6c8c24021ac9d366e453
Joshua Morony <joshua.morony@gmail.com>
2022-06-02 09:27:12 +0930

    set up test data for reddit response

--- src/app/shared/data-access/reddit.service.spec.ts
+++ src/app/shared/data-access/reddit.service.spec.ts
@@ -1,17 +1,57 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { TestBed } from '@angular/core/testing';
 import { SubscriberSpy, subscribeSpyTo } from '@hirez_io/observer-spy';
 import { Gif } from '../interfaces/gif';
+import { RedditPost } from '../interfaces/reddit-post';
+import { RedditResponse } from '../interfaces/reddit-response';
 
 import { RedditService } from './reddit.service';
 
 describe('RedditService', () => {
   let service: RedditService;
   let getGifsSpy: SubscriberSpy<Gif[]>;
+  let testResponse: RedditResponse;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(RedditService);
     getGifsSpy = subscribeSpyTo(service.getGifs());
+
+    const testData: RedditPost = {
+      data: {
+        author: 'Josh',
+        name: 'some cool post',
+        permalink: 'https://google.com',
+        preview: {
+          reddit_video_preview: {
+            is_gif: true,
+            fallback_url: '',
+          },
+        },
+        secure_media: {
+          reddit_video: {
+            is_gif: true,
+            fallback_url: '',
+          },
+        },
+        title: 'some cool post',
+        media: {
+          reddit_video: {
+            is_gif: true,
+            fallback_url: '',
+          },
+        },
+        url: 'https://google.com',
+        thumbnail: '',
+        num_comments: 5,
+      },
+    };
+
+    testResponse = {
+      data: {
+        children: [testData, testData],
+      },
+    };
   });
 
   it('should be created', () => {
@@ -47,5 +87,16 @@ describe('RedditService', () => {
 
       expect(sizeAfter).toBeGreaterThan(sizeBefore);
     });
+  });
+
+  describe('fetchData()', () => {
+    it('should make a request to the reddit API', () => {});
+    it('should return the response from the convertRedditPostToGif method', () => {});
+  });
+
+  describe('getBestSrcForGif()', () => {
+    let testData: RedditPost;
+
+    beforeEach(() => {});
   });
 });
--- src/app/shared/data-access/reddit.service.ts
+++ src/app/shared/data-access/reddit.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Gif } from '../interfaces/gif';
+import { RedditPost } from '../interfaces/reddit-post';
 
 @Injectable({
   providedIn: 'root',
@@ -21,12 +22,31 @@ export class RedditService {
     this.gifs$.next([...this.gifs$.value, ...newData]);
   }
 
-  fetchData() {
+  private fetchData() {
     const dummyData = [
       { title: 'hello', url: 'https://google.com', src: '' },
       { title: 'there', url: 'https://google.com', src: '' },
     ];
 
+    // make request
+    // map(convertRedditPostToGif)
+    // filter(unuseable)
+    // map
+    // .subscribe
+    // next
+
     return dummyData;
+  }
+
+  private convertRedditPostsToGifs(data: RedditPost[]) {
+    // return empty array if response.data.children === 0
+    // convert to Gif format, using best src for gif method
+  }
+
+  private getBestSrcForGif(post: RedditPost) {
+    // if gifv or webm, return mp4
+    // if not, check return media or secure media if it is available
+    // if not, check if a preview is available
+    // otherwise return false
   }
 }
