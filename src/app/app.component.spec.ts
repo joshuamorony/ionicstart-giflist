6eabd37ca27e4e59a0e3e555a24fcd563a68c2df
Joshua Morony <joshua.morony@gmail.com>
2022-06-08 12:13:50 +0930

    call settings service init from app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,15 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { SettingsService } from './shared/data-access/settings.service';
+
+jest.mock('./shared/data-access/settings.service');
 
 describe('AppComponent', () => {
-
   beforeEach(waitForAsync(() => {
-
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [SettingsService],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
@@ -18,6 +20,14 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-  // TODO: add more tests!
 
+  it('should call the init method of the settings service', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.debugElement.componentInstance;
+
+    const settingsService = fixture.debugElement.injector.get(SettingsService);
+    component.ngOnInit();
+
+    expect(settingsService.init).toHaveBeenCalled();
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SettingsService } from './shared/data-access/settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private settingsService: SettingsService) {}
+
+  ngOnInit() {
+    this.settingsService.init();
+  }
 }
