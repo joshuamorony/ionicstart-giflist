4026cdda963ee62b9bcba61fcde7dcc2dc607bc8
Joshua Morony <joshua.morony@gmail.com>
2022-06-08 20:03:51 +0930

    allow modal to be dismissed

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { SettingsService } from '../shared/data-access/settings.service';
 
 import { SettingsComponent } from './settings.component';
@@ -23,7 +23,15 @@ describe('SettingsComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SettingsComponent, MockSettingsFormComponent],
-      providers: [SettingsService],
+      providers: [
+        SettingsService,
+        {
+          provide: ModalController,
+          useValue: {
+            dismiss: jest.fn(),
+          },
+        },
+      ],
       imports: [IonicModule.forRoot(), ReactiveFormsModule],
     }).compileComponents();
 
@@ -47,5 +55,29 @@ describe('SettingsComponent', () => {
     expect(settingsService.save).toHaveBeenCalledWith(
       component.settingsForm.value
     );
+  });
+
+  it('should dismiss when form is saved', () => {
+    const modalController = fixture.debugElement.injector.get(ModalController);
+
+    const settingsForm = fixture.debugElement.query(
+      By.css('app-settings-form')
+    );
+
+    settingsForm.triggerEventHandler('save', null);
+
+    expect(modalController.dismiss).toHaveBeenCalled();
+  });
+
+  it('should be able to dismiss by clicking close button', () => {
+    const modalController = fixture.debugElement.injector.get(ModalController);
+
+    const closeButton = fixture.debugElement.query(
+      By.css('[data-test="modal-close-button"]')
+    );
+
+    closeButton.nativeElement.click();
+
+    expect(modalController.dismiss).toHaveBeenCalled();
   });
 });
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 import { SettingsService } from '../shared/data-access/settings.service';
 
 @Component({
@@ -17,6 +18,7 @@ export class SettingsComponent {
 
   constructor(
     private fb: FormBuilder,
-    public settingsService: SettingsService
+    public settingsService: SettingsService,
+    public modalCtrl: ModalController
   ) {}
 }
